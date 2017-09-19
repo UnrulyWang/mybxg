e1420f00666682574696d58b42252f5bf46f8dae
UnrulyWang <1048859817@qq.com>
2017-09-19 18:23:49 +0800

    模板引擎重构

--- public/js/common.js
+++ public/js/common.js
@@ -1,4 +1,4 @@
-define(['jquery','cookie'],function($){
+define(['jquery','template','cookie'],function($,template){
 	//NProgress.start();
     //
 	//NProgress.done();
@@ -23,12 +23,15 @@ define(['jquery','cookie'],function($){
 //验证用户是否dengl陆
 	var flag=$.cookie('PHPSESSID');
 	console.log(flag);
-	if(!flag){
+	if(!flag && location.pathname!='/main/login'){
 		location.href='/main/login';
 	}
 	var loginInfo=$.cookie('loginInfo');
 	loginInfo=loginInfo&&JSON.parse(loginInfo);
-	$('.aside .profile img').attr('src',loginInfo.tc_avatar);
-	$('.aside .profile h4').html(loginInfo.tc_name);
+	var tpl='<div class="avatar img-circle"><img src="{{tc_avatar}}"></div> <h4>{{tc_name}}</h4>';
+	var html=template.render(tpl,loginInfo);
+	$('.aside .profile').html(html);
+	//$('.aside .profile img').attr('src',loginInfo.tc_avatar);
+	//$('.aside .profile h4').html(loginInfo.tc_name);
 });
 
--- public/js/config.js
+++ public/js/config.js
@@ -5,6 +5,7 @@ require.config({
     baseUrl:'/public/assets',
     paths:{
         jquery: 'jquery/jquery.min',
+        template:'artTemplate/template-web',
         common: '../js/common',
         cookie: 'jquery-cookie/jquery.cookie',
         login: '../js/login'
