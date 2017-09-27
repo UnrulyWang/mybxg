7bf45604fb683f5e9936c3662604e70ee2b7a0fc
UnrulyWang <1048859817@qq.com>
2017-09-27 16:21:23 +0800

    完成了模态框的重用

--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/25.
  */
-define(['jquery','template','util'],function($,template,util){
+define(['jquery','template','util','bootstrap'],function($,template,util){
     //设置导航菜单选中
     util.getLightHigh('/course/add');
     //获取id
@@ -17,6 +17,31 @@ define(['jquery','template','util'],function($,template,util){
             if(data.code==200){
                 var html=template('lessonTpl',data.result);
                 $('#lessonInfo').html(html);
+                //点击添加课时操作
+                $('#addLesson').click(function(){
+
+                    var html=template('modalTpl',{ operate:'添加课时'});
+                    $('#chapterModal').html(html);
+                    $('#chapterModal').modal();
+                });
+                //处理编辑课时操作
+                $('.editLesson').click(function(){
+                    //得到ct_id
+                    var ctId=$(this).attr('data-ctId');
+                    $.ajax({
+                        type:'get',
+                        url:'/api/course/chapter/edit',
+                        data:{ct_id:ctId},
+                        dataType: 'json',
+                         success:function(data){
+                                data.result.operate='编辑课时';
+                             var html=template('modalTpl',data.result);
+                             $('#chapterModal').html(html);
+                             $('#chapterModal').modal();
+                         }
+                    });
+
+                })
             }
         }
     })
