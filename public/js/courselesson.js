5c740e9d6723e49e7e63681cd0edba0ab9f29cf3
UnrulyWang <1048859817@qq.com>
2017-09-27 16:42:12 +0800

    完成了添加和修改课时的表单提交

--- public/js/courselesson.js
+++ public/js/courselesson.js
@@ -1,7 +1,7 @@
 /**
  * Created by Administrator on 2017/9/25.
  */
-define(['jquery','template','util','bootstrap'],function($,template,util){
+define(['jquery','template','util','bootstrap','form'],function($,template,util){
     //设置导航菜单选中
     util.getLightHigh('/course/add');
     //获取id
@@ -19,10 +19,23 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
                 $('#lessonInfo').html(html);
                 //点击添加课时操作
                 $('#addLesson').click(function(){
-
                     var html=template('modalTpl',{ operate:'添加课时'});
                     $('#chapterModal').html(html);
                     $('#chapterModal').modal();
+                    //点击添加按钮 处理添加课时的表单提交
+                  $('#addLesson2').on('click',function(){
+                      $('#lessonForm').ajaxSubmit({
+                          type:'post',
+                          url:'/api/course/chapter/add',
+                          data:{ct_cs_id:csId},
+                          dataType:'json',
+                          success:function(data){
+                              if(data.code==200){
+                                  location.reload();
+                              }
+                          }
+                      });
+                  })
                 });
                 //处理编辑课时操作
                 $('.editLesson').click(function(){
@@ -38,10 +51,26 @@ define(['jquery','template','util','bootstrap'],function($,template,util){
                              var html=template('modalTpl',data.result);
                              $('#chapterModal').html(html);
                              $('#chapterModal').modal();
+                             //处理编辑课时的表单提交
+                             $('#addLesson2').on('click',function(){
+                                 $('#lessonForm').ajaxSubmit({
+                                     type:'post',
+                                     url:'/api/course/chapter/modify',
+                                     data:{ct_cs_id:csId,ct_id:ctId},
+                                     dataType:'json',
+                                     success:function(data){
+                                         if(data.code==200){
+                                             location.reload();
+                                         }
+                                     }
+                                 });
+                             })
                          }
                     });
 
-                })
+                });
+                //处理添加和编辑讲师的表单提交
+                //$()
             }
         }
     })
